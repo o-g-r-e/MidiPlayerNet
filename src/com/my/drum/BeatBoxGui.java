cb81a9f96abe75fa2982b3a0ad22694d4e1918ec
ydroba <ydroba@wiley.com>
2019-08-16 19:04:25 +0300

    beatbox

--- src/com/my/drum/BeatBoxGui.java
+++ src/com/my/drum/BeatBoxGui.java
@@ -28,7 +28,7 @@ public class BeatBoxGui {
 	String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Share", "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo", 
 											"Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "Low-mid Tom", "High Agogo", "Open Hi Conga"	};
 	
-	int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+	
 	
 	public BeatBoxGui() {
 		theFrame = new JFrame("Beat Box");
--- src/com/my/drum/MidiPlayer.java
+++ src/com/my/drum/MidiPlayer.java
@@ -8,22 +8,25 @@ import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.Track;
+import javax.swing.JCheckBox;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.ShortMessage;
 
 public class MidiPlayer {
-	
-	public static final int START_NOTE = 144;
-	public static final int END_NOTE = 128;
-	//public static final int CHANGE_INSTRUMENT = ;
 	private Sequencer sequencer;
+	private Sequence sequence;
+	private Track track;
+	public static final int TICKS_NUM = 16;
+	
+	private int[] instruments = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
 	
 	public MidiPlayer() throws MidiUnavailableException, InvalidMidiDataException {
 		sequencer = MidiSystem.getSequencer();
 		sequencer.open();
-		Sequence sequence = new Sequence(Sequence.PPQ, 4);
+		sequence = new Sequence(Sequence.PPQ, 4);
 		
-		initSimpleTrack(sequence.createTrack());
+		track = sequence.createTrack();
+		initSimpleTrack(track);
 		
 		sequencer.setSequence(sequence);
 		sequencer.setTempoInBPM(220);
@@ -31,14 +34,25 @@ public class MidiPlayer {
 	
 	private void initSimpleTrack(Track track) {
 		for (int i = 5; i < 61; i+=4) {
-			track.add(createEvent(MidiPlayer.START_NOTE, 1, i, 100, i));
-			track.add(createEvent(176, 1, 127, 0, i));
-			track.add(createEvent(MidiPlayer.END_NOTE, 1, i, 100, i+2));
+			track.add(createEvent(ShortMessage.NOTE_ON, 1, i, 100, i));
+			track.add(createEvent(ShortMessage.CONTROL_CHANGE, 1, 127, 0, i)); //пустое событие, которое тригерит ControllerEventListener, благодар€ чему получаем вызов коллбека после каждой ноты
+			track.add(createEvent(ShortMessage.NOTE_OFF, 1, i, 100, i+2));
 		}
 	}
 	
 	private void buildTrack() {
+		int[] trackList = new int[16];
 		
+		sequence.deleteTrack(track);
+		track = sequence.createTrack();
+		
+		for (int i = 0; i < 16; i++) {
+			int key = instruments[i];
+			
+			for (int j = 0; j < MidiPlayer.TICKS_NUM; j++) {
+				JCheckBox jc = 
+			}
+		}
 	}
 	
 	public void setTickEventListener(ControllerEventListener c) {
@@ -51,11 +65,13 @@ public class MidiPlayer {
 		sequencer.start();
 	}
 	
-	public static MidiEvent createEvent(int cmd, int chanel, int a, int b, int tick) {
+	// Examples :
+	// createEvent(start_note, channel_number, note_number, note_sound_volume, tick_play_number)
+	public static MidiEvent createEvent(int command, int channel, int data1, int data2, int tick) {
 		MidiEvent event = null;
 		ShortMessage mes = new ShortMessage();
 		try {
-			mes.setMessage(cmd, chanel, a, b);
+			mes.setMessage(command, channel, data1, data2);
 			event = new MidiEvent(mes, tick);
 		} catch (InvalidMidiDataException e) {
 			e.printStackTrace();
